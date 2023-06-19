742180a6ebf63ded71cb616d12b46dbded21a555
Patrik <rambo3swe@gmail.com>
2023-06-19 16:16:19 +0200

    feat: added password protected logs support

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"sync"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"github.com/oklog/ulid/v2"
 )
 
@@ -34,6 +36,26 @@ func main() {
 			"urllogs": "/log/" + id.String() + "/logs",
 		})
 	})
+
+	r.POST("/log/:id/password", func(c *gin.Context) {
+		setPass := SetPassword{}
+		err := c.MustBindWith(&setPass, binding.JSON)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = reqLogs.AddPassword(c.Param("id"), setPass.Password)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status": "accepted",
+		})
+	})
+
 	r.Any("/log/:id", func(c *gin.Context) {
 
 		reqLog := RequestLog{}
@@ -61,30 +83,51 @@ func main() {
 			"status": "accepted",
 		})
 	})
-	r.GET("/log/:id/logs", func(c *gin.Context) {
-
-		logs := reqLogs.Get(c.Param("id"))
-
-		logsJson, err := json.Marshal(logs)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	r.POST("/log/:id/logs", func(c *gin.Context) {
+		if !reqLogs.CheckPassword(c.Param("id"), c.PostForm("password")) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid password"})
 			return
 		}
 
-		c.HTML(http.StatusOK, "logs.tmpl", gin.H{"logs": logs, "logs_json": logsJson})
+		renderLogs(c, reqLogs)
+	})
+	r.GET("/log/:id/logs", func(c *gin.Context) {
+
+		if reqLogs.HasPassword(c.Param("id")) {
+			c.HTML(http.StatusOK, "password.tmpl", gin.H{
+				"id": c.Param("id"),
+			})
+			return
+		}
+
+		renderLogs(c, reqLogs)
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
+func renderLogs(c *gin.Context, reqLogs *RequestLogs) {
+	logs := reqLogs.Get(c.Param("id"))
+
+	logsJson, err := json.Marshal(logs)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.HTML(http.StatusOK, "logs.tmpl", gin.H{"logs": logs, "logs_json": logsJson})
+}
+
 type RequestLogs struct {
-	mu   sync.Mutex
-	logs map[string][]RequestLog
+	mu        sync.Mutex
+	logs      map[string][]RequestLog
+	passwords map[string]string
 }
 
 func NewRequestLogs() *RequestLogs {
 	return &RequestLogs{
-		mu:   sync.Mutex{},
-		logs: map[string][]RequestLog{},
+		mu:        sync.Mutex{},
+		logs:      map[string][]RequestLog{},
+		passwords: map[string]string{},
 	}
 }
 
@@ -95,6 +138,37 @@ func (r *RequestLogs) Add(id string, reqLog RequestLog) {
 	//	r.logs[id] = []RequestLog{}
 	//}
 	r.logs[id] = append(r.logs[id], reqLog)
+}
+
+func (r *RequestLogs) AddPassword(id string, password string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, ok := r.passwords[id]; ok {
+		return fmt.Errorf("password already set")
+	}
+
+	r.passwords[id] = password
+	return nil
+}
+
+func (r *RequestLogs) HasPassword(id string) bool {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	_, ok := r.passwords[id]
+	return ok
+}
+
+func (r *RequestLogs) CheckPassword(id string, password string) bool {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if _, ok := r.passwords[id]; !ok {
+		return true
+	}
+
+	return r.passwords[id] == password
 }
 
 func (r *RequestLogs) Get(id string) []RequestLog {
@@ -108,4 +182,8 @@ type RequestLog struct {
 	Body    string
 	URL     *url.URL
 	Method  string
+}
+
+type SetPassword struct {
+	Password string `json:"password"`
 }
